5f966bf3c61e4a487b4181ca8b49492390eeb732
takatsugu ishikawa <tissi0708@gmail.com>
2013-08-22 22:40:33 +0900

    Initial Commit

---
+++ cl/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  cl
+//
+//  Created by ishi on 2013/08/22.
+//  Copyright (c) 2013年 simultechnology. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
